bc20d7256661a99c7a0fb381f971a1b2f156e82a
Oliver Thiele <mail@oliver-thiele.de>
2025-01-04 11:15:29 +0100

    [TASK] Update code for TYPO3 v13 compatibility
    - Introduced a new icon for the content element.
    - Updated TCA configuration
    - Updated language files with translations and descriptions.
    - Created a new SVG icon file for better visual representation.

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'icon-ot-cefluidtemplate' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:ot_cefluidtemplates/Resources/Public/Icons/CeFluidtemplate.svg',
+    ]
+];
